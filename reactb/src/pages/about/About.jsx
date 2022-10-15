32db87c663b00218756962b7e38b1a3d48f8eacf
ABHIJITH R B <107575528+knoskimo@users.noreply.github.com>
2022-10-15 12:58:45 +0530

    Update About.jsx

--- reactb/src/pages/about/About.jsx
+++ reactb/src/pages/about/About.jsx
@@ -3,14 +3,17 @@ import "./about.css"
 
 export default function About() {
   return (
-    <div className='abouta'>
+    <div className='aboutw'>
+      <div className='abouta'>
         <h1 className='abouth'>About the Blog</h1>
         <p className='aboutd'>
             <b>
                 Series blog is dedicated for writing reviews and updating latest information on TV & Web Series
             </b>
         </p>
+     </div>
     </div>
   )
 }
+
 
